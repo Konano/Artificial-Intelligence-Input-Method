9086e10f5550fdc3a4b369f05b6151fbb1172d5d
Konano <nanoapezlk@gmail.com>
2018-03-22 10:14:19 +0800

    init

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+#include <cstring>
+
+using namespace std;
+
+char s[5009];
+
+int main()
+{
+	scanf("%s", s);
+	
+	return 0;
+}
---
+++ training.cpp
@@ -0,0 +1,14 @@
+#include <cstdio>
+#include <cstring>
+#include "jsoncpp/json.h"
+
+using namespace std;
+
+char s[5009];
+
+int main()
+{
+	scanf("%s", s);
+	
+	return 0;
+}
